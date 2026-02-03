0d69dc1ae467f924073ebdb0d09eff38ee095d40
Kyunghwan Kim <kyunghwan.kim@motov.co.kr>
2026-02-03 14:41:36 +0900

    dynamic_widget

--- dynamic_widget/lib/dynamic_widget.dart
+++ dynamic_widget/lib/dynamic_widget.dart
@@ -1,5 +1,3 @@
-export 'package:dynamic_widget_by_zpdl/src/painting/dynamic_edge_insets.dart';
-
-export 'package:dynamic_widget_by_zpdl/src/services/dynamic_system_ui_overlay_style.dart';
-
-export 'package:dynamic_widget_by_zpdl/src/widget/dynamic_async_worker.dart';
+export 'package:dynamic_widget/src/painting/dynamic_edge_insets.dart';
+export 'package:dynamic_widget/src/services/dynamic_system_ui_overlay_style.dart';
+export 'package:dynamic_widget/src/widget/dynamic_async_worker.dart';
